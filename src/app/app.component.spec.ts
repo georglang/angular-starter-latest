66cfb830d107cac518c0624c7f732673139b158c
Georg Lang <georglang0788@gmx.net>
2024-03-10 16:34:26 +0100

    chore(typo): fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'startet-latest' title`, () => {
+  it(`should have the 'starter-latest' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('startet-latest');
+    expect(app.title).toEqual('starter-latest');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, startet-latest');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, starter-latest');
   });
 });
